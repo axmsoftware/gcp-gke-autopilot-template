3919dc0d360c900e8fd96720fef0ab5e6608a0c3
AXM Software <35903292+axmsoftware@users.noreply.github.com>
2022-04-14 23:32:54 -0700

    Use autocloud module only

--- autocloud.tf
+++ autocloud.tf
@@ -6,44 +6,44 @@ module "autocloud-service-account" {
   project_id = "axm-gke-autopilot-starter"
 }
 # From terraformer
-resource "google_service_account" "axm-gcp-autocloud-sa-read" {
-  account_id   = "axm-gcp-autocloud-sa-read"
-  description  = "Read Only Service Account for integration between GCP and Autocloud"
-  disabled     = "false"
-  display_name = "axm-gcp-autocloud-sa-read"
-  project      = "axm-gke-autopilot-starter"
-}
+# resource "google_service_account" "axm-gcp-autocloud-sa-read" {
+#   account_id   = "axm-gcp-autocloud-sa-read"
+#   description  = "Read Only Service Account for integration between GCP and Autocloud"
+#   disabled     = "false"
+#   display_name = "axm-gcp-autocloud-sa-read"
+#   project      = "axm-gke-autopilot-starter"
+# }
 # GKE Details?
-resource "google_project_iam_member" "axm-gcp-autocloud-sa-read-cluster-viewer" {
-  member  = "serviceAccount:axm-gcp-autocloud-sa-read@axm-gke-autopilot-starter.iam.gserviceaccount.com"
-  project = "axm-gke-autopilot-starter"
-  role    = "roles/container.clusterViewer"
-}
-resource "google_project_iam_member" "axm-gcp-autocloud-sa-read-container-viewer" {
-  member  = "serviceAccount:axm-gcp-autocloud-sa-read@axm-gke-autopilot-starter.iam.gserviceaccount.com"
-  project = "axm-gke-autopilot-starter"
-  role    = "roles/container.viewer"
-}
+# resource "google_project_iam_member" "axm-gcp-autocloud-sa-read-cluster-viewer" {
+#   member  = "serviceAccount:axm-gcp-autocloud-sa-read@axm-gke-autopilot-starter.iam.gserviceaccount.com"
+#   project = "axm-gke-autopilot-starter"
+#   role    = "roles/container.clusterViewer"
+# }
+# resource "google_project_iam_member" "axm-gcp-autocloud-sa-read-container-viewer" {
+#   member  = "serviceAccount:axm-gcp-autocloud-sa-read@axm-gke-autopilot-starter.iam.gserviceaccount.com"
+#   project = "axm-gke-autopilot-starter"
+#   role    = "roles/container.viewer"
+# }
 # Autocloud Default
-resource "google_project_iam_member" "axm-gcp-autocloud-sa-read-security-reviewer" {
-  member  = "serviceAccount:axm-gcp-autocloud-sa-read@axm-gke-autopilot-starter.iam.gserviceaccount.com"
-  project = "axm-gke-autopilot-starter"
-  role    = "roles/iam.securityReviewer"
-}
-resource "google_project_iam_member" "axm-gcp-autocloud-sa-read-service-usage-viewer" {
-  member  = "serviceAccount:axm-gcp-autocloud-sa-read@axm-gke-autopilot-starter.iam.gserviceaccount.com"
-  project = "axm-gke-autopilot-starter"
-  role    = "roles/serviceusage.serviceUsageViewer"
-}
-resource "google_project_iam_member" "axm-gcp-autocloud-sa-read-stackdriver-accounts-viewer" {
-  member  = "serviceAccount:axm-gcp-autocloud-sa-read@axm-gke-autopilot-starter.iam.gserviceaccount.com"
-  project = "axm-gke-autopilot-starter"
-  role    = "roles/stackdriver.accounts.viewer"
-}
+# resource "google_project_iam_member" "axm-gcp-autocloud-sa-read-security-reviewer" {
+#   member  = "serviceAccount:axm-gcp-autocloud-sa-read@axm-gke-autopilot-starter.iam.gserviceaccount.com"
+#   project = "axm-gke-autopilot-starter"
+#   role    = "roles/iam.securityReviewer"
+# }
+# resource "google_project_iam_member" "axm-gcp-autocloud-sa-read-service-usage-viewer" {
+#   member  = "serviceAccount:axm-gcp-autocloud-sa-read@axm-gke-autopilot-starter.iam.gserviceaccount.com"
+#   project = "axm-gke-autopilot-starter"
+#   role    = "roles/serviceusage.serviceUsageViewer"
+# }
+# resource "google_project_iam_member" "axm-gcp-autocloud-sa-read-stackdriver-accounts-viewer" {
+#   member  = "serviceAccount:axm-gcp-autocloud-sa-read@axm-gke-autopilot-starter.iam.gserviceaccount.com"
+#   project = "axm-gke-autopilot-starter"
+#   role    = "roles/stackdriver.accounts.viewer"
+# }
 
-resource "google_project_iam_member" "axm-gcp-autocloud-sa-read-viewer" {
-  member  = "serviceAccount:axm-gcp-autocloud-sa-read@axm-gke-autopilot-starter.iam.gserviceaccount.com"
-  project = "axm-gke-autopilot-starter"
-  role    = "roles/viewer"
-}
+# resource "google_project_iam_member" "axm-gcp-autocloud-sa-read-viewer" {
+#   member  = "serviceAccount:axm-gcp-autocloud-sa-read@axm-gke-autopilot-starter.iam.gserviceaccount.com"
+#   project = "axm-gke-autopilot-starter"
+#   role    = "roles/viewer"
+# }
 
